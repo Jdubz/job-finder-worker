24c4763e9d011afafdb608b86b8a14f1d33a1365
josh <contact@joshwentworth.com>
2025-11-23 22:17:25 -0800

    Fix content_items slim migration for tests

--- infra/sqlite/schema.sql
+++ infra/sqlite/schema.sql
@@ -4,7 +4,4 @@
 .read migrations/003_queue_type_extensions.sql
 .read migrations/004_generator_workflow.sql
 .read migrations/005_content_items_slim.sql
-.read migrations/005_content_items_unify.sql
 .read migrations/006_drop_contact_submissions.sql
-.read migrations/006_drop_userid_visibility_content_items.sql
-.read migrations/007_rebuild_content_items_without_userid.sql
