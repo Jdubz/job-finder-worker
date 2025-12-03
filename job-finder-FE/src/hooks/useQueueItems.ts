7b47110a153c9154d68ce6cd13f6978bd5d89fc0
josh <contact@joshwentworth.com>
2025-12-02 22:05:54 -0800

    FE: add allowReanalysis to submit company params

--- job-finder-FE/src/hooks/useQueueItems.ts
+++ job-finder-FE/src/hooks/useQueueItems.ts
@@ -14,6 +14,7 @@ interface SubmitCompanyParams {
   companyName: string
   websiteUrl?: string
   companyId?: string | null
+  allowReanalysis?: boolean
 }
 
 interface SubmitSourceDiscoveryParams {
